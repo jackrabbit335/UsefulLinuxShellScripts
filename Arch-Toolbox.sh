e6d34aaf7bcb3801965f4770e0fed3277270ed03
James <jackharkness444@protonmail.com>
2018-08-18 01:37:42 -0500

    Update Arch-Toolbox.sh

--- Arch-Toolbox.sh
+++ Arch-Toolbox.sh
@@ -46,7 +46,8 @@ Setup() {
             			sudo iptables -A INPUT -p tcp --dport telnet -j DROP
 			break
 			done
-            		sudo /sbin/iptables-save && sudo systemctl restart iptables
+			#Save the current set of rules to a text file for future use and then restart the service
+            		sudo /sbin/iptables-save > first-iptables-rules.txt && sudo systemctl restart iptables
         	fi
     	fi
 
