1b27714f1770c4b5284b2a9f304ad4820029d80a
James <jackharkness444@protonmail.com>
2018-08-18 03:18:03 -0500

    Update Arch-Toolbox.sh

--- Arch-Toolbox.sh
+++ Arch-Toolbox.sh
@@ -45,10 +45,13 @@ Setup() {
 				sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 				sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 				sudo iptables -A INPUT -p tcp --dport 143 -j ACCEPT
+				sudo iptables -A INPUT -p tcp --dport 993 -j ACCEPT
 				sudo iptables -A INPUT -p tcp --dport 110 -j ACCEPT
+				sudo iptables -A INPUT -p tcp --dport 995 -j ACCEPT
 				sudo iptables -A INPUT -p tcp --dport 20 -j ACCEPT
 				sudo iptables -A INPUT -p tcp --dport 21 -j ACCEPT
 				sudo iptables -A INPUT -p tcp --dport 25 -j ACCEPT
+				sudo iptables -A INPUT -p tcp --dport 465 -j ACCEPT
 				sudo iptables -A INPUT -p tcp --dport 22 -j DROP
 				sudo iptables -A INPUT -p tcp --dport 23 -j DROP
 				sudo iptables -A INPUT -p icmp -j DROP
@@ -60,12 +63,15 @@ Setup() {
 				sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 				sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 				sudo iptables -A INPUT -p tcp --dport 143 -j ACCEPT
+				sudo iptables -A INPuT -p tcp --dport -993 -j ACCEPT
 				sudo iptables -A INPUT -p tcp --dport 110 -j ACCEPT
+				sudo iptables -A INPUT -p tcp --dport 995 -j ACCEPT
 				sudo iptables -A INPUT -p tcp --dport 20 -j ACCEPT
 				sudo iptables -A INPUT -p tcp --dport 21 -j ACCEPT
 				sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 				sudo iptables -A INPUT -p tcp --dport 23 -j ACCEPT
 				sudo iptables -A INPUT -p tcp --dport 25 -j ACCEPT
+				sudo iptables -A INPUT -p tcp --dport 465 -j ACCEPT
 				sudo iptables -P INPUT -p tcp -j DROP
 				#Save the current set of rules to a text file for future use and then restart the service
             			sudo iptables-save > first-iptables-rules.dat && sudo systemctl restart iptables
