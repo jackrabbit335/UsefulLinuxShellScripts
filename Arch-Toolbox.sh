6714c4020d6baa980409f47adfc806deb5162071
James <jackharkness444@protonmail.com>
2018-08-26 21:25:23 -0500

    Update Arch-Toolbox.sh

--- Arch-Toolbox.sh
+++ Arch-Toolbox.sh
@@ -1635,7 +1635,7 @@ _EOF_
 		echo "Please select the device from the list"
 		read device
 		sudo mount $device /mnt 
-		sudo rsync -aAXv --delete --exclude={"*.cache/*","*.thumbnails/*"."*/.local/share/Trash/*"}  /mnt/$host-backups/* /home
+		sudo rsync -aAXv --delete --exclude={"*.cache/*","*.thumbnails/*"."*/.local/share/Trash/*"}  /mnt/$host-backups/* /home/$USER
 		sudo sync 
 		Restart
 	elif [[ $Mountpoint == /run/media/$USER/* ]];
