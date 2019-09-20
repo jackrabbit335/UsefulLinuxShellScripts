ab1840f0af9a1cfbb38449231edfe9cef4b19098
James <jackharkness444@protonmail.com>
2019-09-20 01:11:43 -0500

    Add files via upload
    As is no longer necessary have removed some useless features such as removing hashes etc and have improved on the final look of the hosts file and added solid sources to be mirrored here at least for the time being. To add more domains see blacklist.txt. To add more hosts files or review the ones already listed see sources.txt. Tweaked checking for hosts.bak and how it applies the backup file in place of the original in the event that it exists. Previously this used a while loop and then did a separate operation to merge the backup file with the original, this is now all done as one operation which should make it more seamless. Readded some hosts files from back before. Removed an unnecessary warning label as it is just not needed with the dialogue at the beginning of the script.

--- Hostsman4linux.sh
+++ Hostsman4linux.sh
@@ -10,69 +10,62 @@ _EOF_
 
 #This updates the hosts file
 
-echo "WARNING! USE OF THESE HOSTS COULD CAUSE MANY OF YOUR FAVORITE SITES TO CEASE FUNCTIONING. PROCEED WITH CAUTION."
-
 echo "searching for /etc/hosts.bak and then creating hosts file to block tracking"
 find /etc/hosts.bak
-while [ $? -gt 0 ]
-do
+if [ $? -gt 0 ]
+then
 	sudo cp /etc/hosts /etc/hosts.bak
-break
-done
-
-sudo cp /etc/hosts.bak /etc/hosts
+else
+	sudo cp /etc/hosts.bak /etc/hosts
+fi
 
 cd /tmp
-echo "----------------Hostsman4linux--------------" >> adblock
+echo "---------------------Hostsman4linux-------------------" >> adblock
 
-str1=http://winhelp2002.mvps.org/hosts.txt
-str2=https://someonewhocares.org/hosts/hosts
+str1=https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/Hosts%20%26%20sourcelist/MVPShosts
+str2=https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/Hosts%20%26%20sourcelist/Someonewhocares
 str3=https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/Hosts%20%26%20sourcelist/Peteradslist
-str4=http://www.malwaredomainlist.com/hostslist/hosts.txt
-str5=https://hosts-file.net/ad_servers.txt
+str4=https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/Hosts%20%26%20sourcelist/Malwarehosts
+str5=https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/Hosts%20%26%20sourcelist/Coinblock
 str6=https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/Hosts%20%26%20sourcelist/AdAway
 str7=https://github.com/jackrabbit335/UsefulLinuxShellScripts/blob/master/Hosts%20%26%20sourcelist/blacklist.txt
-str8=http://www.sysctl.org/cameleon/hosts
+str8=https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/Hosts%20%26%20sourcelist/Sysctlhosts
+str9=https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/Hosts%20%26%20sourcelist/blacklist.txt
+str10=https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/Hosts%20%26%20sourcelist/bjornhosts
 
 
 while getopts :ABCDEFG option; do
 	case $option in
-		A) wget $str1 && cat hosts.txt >> adblock && rm hosts.txt
+		A) wget $str1 && cat MVPShosts >> adblock && rm MVPShosts
 		;;
-		B) wget $str2 && cat hosts >> adblock && rm hosts
+		B) wget $str2 && cat Someonewhocares >> adblock && rm Someonewhocares
 		;;
 		C) wget $str3 && cat Peteradslist >> adblock && rm Peteradslist
 		;;
-		D) wget $str4 && cat hosts.txt >> adblock && rm hosts.txt
+		D) wget $str4 && cat Malwarehosts >> adblock && rm Malwarehosts
 		;;
-		E) wget $str5 && cat ad_servers.txt >> adblock && rm ad_servers.txt
+		E) wget $str5 && cat Coinblock >> adblock && rm Coinblock
 		;;
 		F) wget $str6 && cat AdAway >> adblock && rm AdAway
 		;;
 		G) wget $str7 && cat blacklist.txt >> adblock && rm blacklist.txt
 		;;
-		H) wget $str8 && cat hosts >> adblock && rm hosts
+		H) wget $str8 && cat Sysctlhosts >> adblock && rm Sysctlhosts
+		;;
+		I) wget $str9 && cat blacklist.txt >> adblock && rm blacklist.txt
+		;;
+		J) wget $str10 && cat bjornhosts >> adblock && rm bjornhosts
 		;;
 		*)
 	esac
 done
 
-echo "----------------Hostsman4linux--------------" >> adblock
+echo "---------------------Hostsman4linux-------------------" >> adblock
 
 #This tries to deduplicate if multiple files were used.
 if [[ $# -gt 1 ]]; then
 	sort adblock | uniq -u | sort -r > adblock.new && mv adblock.new adblock
 fi
-
-#This ensures that we are using All 127.x for pointing back to home
-sed -i 's/127.0.0.1/0.0.0.0 /g' adblock
-
-#Remove comments and spaces
-sed -e '/^[[:space:]]*$/d' adblock > adblock.new && mv adblock.new adblock
-sed -e 's/[[:blank:]]//g' adblock > adblock.new && mv adblock.new adblock
-sed -e 's/0.0.0.0/0.0.0.0  /g' adblock > adblock.new && mv adblock.new adblock
-sed -e '/#.*/d' adblock > adblock.new && mv adblock.new adblock
-sed -e '/^*$/d' adblock > adblock.new && mv adblock.new adblock
 
 #This merges adblock with /etc/hosts then removes hosts
 echo "" | sudo tee -a /etc/hosts
