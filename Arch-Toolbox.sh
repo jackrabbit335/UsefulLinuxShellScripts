d942397d4814dcbc0319ebe01b94ede5252040ee
James <jackharkness444@protonmail.com>
2020-02-23 05:58:37 -0600

    Update Arch-Toolbox.sh
    Fix Swap creation issue

--- Arch-Toolbox.sh
+++ Arch-Toolbox.sh
@@ -1771,8 +1771,7 @@ MakeSwap(){
 	grep -q "swap" /etc/fstab
 	if [ $? -eq 0 ];
 	then
-		sudo cp /etc/fstab /etc/fstab.old; sudo fallocate --length 2G /swapfile; sudo chmod 600 /swapfile; 
-		sudo mkswap /swapfile; sudo swapon /swapfile
+		sudo cp /etc/fstab /etc/fstab.old; sudo fallocate --length 4G /swapfile; sudo chmod 600 /swapfile; sudo mkswap /swapfile; sudo swapon /swapfile
 		echo "/swapfile swap swap sw 0 0" | sudo tee -a /etc/fstab
 	else
 		echo "Swap was already there so there is nothing to do"
