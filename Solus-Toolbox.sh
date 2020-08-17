2468e6aa6a872d8f35b60cb97cec9a005987be9c
James <jackharkness444@protonmail.com>
2020-08-17 02:40:29 -0500

    Update Solus-Toolbox.sh

--- Solus-Toolbox.sh
+++ Solus-Toolbox.sh
@@ -646,7 +646,7 @@ InstallAndConquer(){
 			sudo snap install chromium
 		elif [[ $browser == 9 ]];
 		then
-			wget https://storage-waterfox.netdna-ssl.com/releases/linux64/installer/waterfox-classic-2020.07.2.en-US.linux-x86_64.tar.bz2; tar -xvjf waterfox-classic-2020.07.1.en-US.linux-x86_64.tar.bz2; sudo mv waterfox /opt && sudo ln -s /opt/waterfox/waterfox /usr/bin/waterfox
+			wget https://storage-waterfox.netdna-ssl.com/releases/linux64/installer/waterfox-classic-2020.07.2.en-US.linux-x86_64.tar.bz2; tar -xvjf waterfox-classic-2020.07.2.en-US.linux-x86_64.tar.bz2; sudo mv waterfox /opt && sudo ln -s /opt/waterfox/waterfox /usr/bin/waterfox
 			wget https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/waterfox.desktop; sudo mv waterfox.desktop /usr/share/applications/waterfox.desktop
 		elif [[ $browser == 10 ]];
 		then
