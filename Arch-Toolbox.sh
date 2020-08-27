e945bae0059337f5d565850cebda2edf91a1e39c
James <jackharkness444@protonmail.com>
2020-08-27 07:31:30 -0500

    Update Arch-Toolbox.sh
    Added /etc/host.conf to backed up system files.

--- Arch-Toolbox.sh
+++ Arch-Toolbox.sh
@@ -8,6 +8,7 @@ Setup(){
 	sudo cp /etc/systemd/journald.conf /etc/systemd/journald.conf.bak
 	sudo cp /etc/default/grub /etc/default/grub.bak
 	sudo cp /etc/profile /etc/profile.bak
+	sudo cp /etc/host.conf /etc/host.conf.bak
 	sudo cp /etc/systemd/coredump.conf /etc/systemd/coredump.conf.bak
 	sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 	sudo cp /etc/passwd /etc/passwd.bak
