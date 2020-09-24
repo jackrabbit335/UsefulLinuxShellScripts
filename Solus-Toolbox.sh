d63a38c757a153a2c86cd600edae563b4c84d2fd
James <jackharkness444@protonmail.com>
2020-09-24 14:16:48 -0500

    Update Solus-Toolbox.sh
    Updated Waterfox Link

--- Solus-Toolbox.sh
+++ Solus-Toolbox.sh
@@ -692,7 +692,7 @@ InstallAndConquer(){
 				sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/network/web/browser/google-chrome-stable/pspec.xml; sudo eopkg it google-chrome-*.eopkg;sudo rm google-chrome-*.eopkg
 			elif [[ $browser == 9 ]];
 			then
-				wget https://storage-waterfox.netdna-ssl.com/releases/linux64/installer/waterfox-classic-2020.08.1.en-US.linux-x86_64.tar.bz2; tar -xvjf waterfox-classic-2020.08.1.en-US.linux-x86_64.tar.bz2; sudo mv waterfox /opt && sudo ln -s /opt/waterfox/waterfox /usr/bin/waterfox
+				wget https://storage-waterfox.netdna-ssl.com/releases/linux64/installer/waterfox-classic-2020.09.en-US.linux-x86_64.tar.bz2; tar -xvjf waterfox-classic-2020.09.en-US.linux-x86_64.tar.bz2; sudo mv waterfox /opt && sudo ln -s /opt/waterfox/waterfox /usr/bin/waterfox
 				wget https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/waterfox.desktop; sudo mv waterfox.desktop /usr/share/applications/waterfox.desktop
 			elif [[ $browser == 10 ]];
 			then
