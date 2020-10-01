71bf6d19244776a308279f147e26b373db720d25
James <jackharkness444@protonmail.com>
2020-09-30 20:22:51 -0500

    Update Hostsman4linux.sh
    Added Bjorn Stromberg's hosts

--- Hostsman4linux.sh
+++ Hostsman4linux.sh
@@ -31,8 +31,10 @@ str8=https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/mas
 str9=https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/Hosts%20%26%20sourcelist/Badd-Boyz
 str10=https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/Hosts%20%26%20sourcelist/Gambling
 str11=https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/Hosts%20%26%20sourcelist/Tomslist
+str12=https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/Hosts%20%26%20sourcelist/bjornhosts
 
-while getopts :ABCDEFGHIJK option; do
+
+while getopts :ABCDEFGHIJKL option; do
 	case $option in
 		A) wget $str1 && cat MVPShosts >> adblock && rm MVPShosts
 		;;
@@ -55,6 +57,8 @@ while getopts :ABCDEFGHIJK option; do
 		J) wget $str10 && cat Gambling >> adblock && rm Gambling
 		;;
 		K) wget $str11 && cat Tomslist >> adblock && rm Tomslist
+		;;
+		L) wget $str12 && cat bjornhosts >> adblock && rm bjornhosts
 		;;
 		*)
 	esac
