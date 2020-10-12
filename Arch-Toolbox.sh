d948d9a5341146504c5a2081dda6b92f85142fe6
James <jackharkness444@protonmail.com>
2020-10-11 23:27:44 -0500

    Update Arch-Toolbox.sh
    Updated Palemoon link

--- Arch-Toolbox.sh
+++ Arch-Toolbox.sh
@@ -771,7 +771,7 @@ EOF
 				wget https://aur.archlinux.org/cgit/aur.git/snapshot/vivaldi.tar.gz; gunzip vivaldi.tar.gz; tar -xvf vivaldi.tar; cd vivaldi && makepkg -si
 			elif [[ $browser == 7 ]];
 			then
-				wget linux.palemoon.org/datastore/release/palemoon-28.14.0.linux-x86_64.tar.xz; tar -xf palemoon-28.14.0.linux-x86_64.tar.xz; sudo ln -s ~/palemoon/palemoon /usr/bin/palemoon
+				wget linux.palemoon.org/datastore/release/palemoon-28.14.2.linux-x86_64.tar.xz; tar -xf palemoon-28.14.2.linux-x86_64.tar.xz; sudo ln -s ~/palemoon/palemoon /usr/bin/palemoon
 				wget https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/palemoon.desktop; sudo mv palemoon.desktop /usr/share/applications/palemoon.desktop
 			elif [[ $browser == 8 ]];
 			then
