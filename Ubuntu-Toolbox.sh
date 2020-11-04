9912af72f51644f0e874e1abf464828d54cbfc8d
James <VeilofMaya@vivaldi.net>
2020-11-04 03:35:17 -0600

    Update Ubuntu-Toolbox.sh
    Added etc bash file to important system files to back up
    Updated links to Pale Moon Waterfox and Vivaldi

--- Ubuntu-Toolbox.sh
+++ Ubuntu-Toolbox.sh
@@ -12,6 +12,7 @@ Setup(){
 	sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 	sudo cp /etc/environment /etc/environment.bak
 	sudo cp /etc/profile /etc/profile.bak
+	sudo cp /etc/bash.bashrc /etc/bash.bashrc.bak
 	sudo cp /etc/default/grub /etc/default/grub.bak
 	sudo cp /etc/fstab /etc/fstab.bak
 	sudo cp /etc/passwd /etc/passwd.bak
@@ -977,11 +978,11 @@ InstallAndConquer(){
 				sudo apt install -f
 			elif [[ $browser == 6 ]];
 			then
-				wget http://linux.palemoon.org/datastore/release/palemoon-28.14.2.linux-x86_64.tar.xz; tar -xvf palemoon-28.14.2.linux-x86_64.tar.xz; sudo ln -s ~/palemoon/palemoon /usr/bin/palemoon
+				wget http://linux.palemoon.org/datastore/release/palemoon-28.15.0.linux-x86_64.tar.xz; tar -xvf palemoon-28.15.0.linux-x86_64.tar.xz; sudo ln -s ~/palemoon/palemoon /usr/bin/palemoon
 				wget https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/palemoon.desktop; sudo mv palemoon.desktop /usr/share/applications/palemoon.desktop
 			elif [[ $browser == 7 ]];
 			then
-				wget https://downloads.vivaldi.com/stable/vivaldi-stable_3.3.2022.47-1_amd64.deb; sudo apt install -f
+				wget https://downloads.vivaldi.com/stable/vivaldi-stable_3.4.2066.94-1_amd64.deb; sudo apt install -f
 			elif [[ $browser == 8 ]];
 			then
 				sudo sh -c 'echo "deb http://deb.opera.com/opera/ stable non-free" >> /etc/apt/sources.list.d/opera.list'; sudo sh -c 'wget -O - http://deb.opera.com/archive.key | apt-key add -'
@@ -994,7 +995,7 @@ InstallAndConquer(){
 				sudo apt install -y dillo
 			elif [[ $browser == 11 ]];
 			then
-				wget https://cdn.waterfox.net/releases/linux64/installer/waterfox-classic-2020.09.en-US.linux-x86_64.tar.bz2; tar -xvf waterfox-classic-2020.09.en-US.linux-x86_64.tar.bz2; sudo mv waterfox /opt && sudo ln -s /opt/waterfox/waterfox /usr/bin/waterfox
+				wget https://cdn.waterfox.net/releases/linux64/installer/waterfox-classic-2020.10.en-US.linux-x86_64.tar.bz2; tar -xvf waterfox-classic-2020.10.en-US.linux-x86_64.tar.bz2; sudo mv waterfox /opt && sudo ln -s /opt/waterfox/waterfox /usr/bin/waterfox
 				wget https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/waterfox.desktop; sudo mv waterfox.desktop /usr/share/applications/waterfox.desktop
 			elif [[ $browser == 12 ]];
 			then
