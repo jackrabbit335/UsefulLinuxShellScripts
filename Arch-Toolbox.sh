9267a324dd181982f4721b6a55fb36fa4ffe2498
James <VeilofMaya@vivaldi.net>
2021-02-03 14:18:33 -0600

    Add files via upload
    Updated Palemoon it recently started packaging GTK3 tarballs so that's what we're using.

--- Arch-Toolbox.sh
+++ Arch-Toolbox.sh
@@ -850,7 +850,7 @@ InstallAndConquer(){
 				wget https://aur.archlinux.org/cgit/aur.git/snapshot/vivaldi-snapshot.tar.gz; gunzip vivaldi-snapshot.tar.gz; tar -xvf vivaldi-snapshot.tar; cd vivaldi-snapshot && makepkg -si
 			elif [[ $browser == 8 ]];
 			then
-				wget linux.palemoon.org/datastore/release/palemoon-28.17.0.linux-x86_64.tar.xz; tar -xf palemoon-28.17.0.linux-x86_64.tar.xz; sudo ln -s ~/palemoon/palemoon /usr/bin/palemoon
+				wget http://linux.palemoon.org/datastore/release/palemoon-29.0.0.linux-x86_64-gtk3.tar.xz; tar -xf palemoon-29.0.0.linux-x86_64-gtk3.tar.xz; sudo ln -s ~/palemoon/palemoon /usr/bin/palemoon
 				wget https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/palemoon.desktop; sudo mv palemoon.desktop /usr/share/applications/palemoon.desktop
 			elif [[ $browser == 9 ]];
 			then
@@ -1690,7 +1690,7 @@ cleanup(){
 	sudo pacman -Rsn --noconfirm $(pacman -Qqdt)
 
 	#Optional This will remove the pacman cached applications and older versions
-	#sudo pacman -Scc
+	sudo pacman -Scc
 
 	#This removes unwanted apps
 	Uninstall
@@ -2050,7 +2050,6 @@ Reset(){
 
 DriverManager(){
 	echo "Coming Soon!"
-	sleep 1
 
 	clear
 	Greeting
