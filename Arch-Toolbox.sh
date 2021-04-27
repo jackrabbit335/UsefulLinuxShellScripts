6100447991d91ec392901ae2f689d268fe64c1a6
James <VeilofMaya@vivaldi.net>
2021-04-26 20:04:13 -0500

    Add files via upload
    Updated Waterfox and fixed palemoon installation

--- Arch-Toolbox.sh
+++ Arch-Toolbox.sh
@@ -762,7 +762,19 @@ InstallAndConquer(){
 			;;
 			7)
 			echo "This installs cleaning software for Arch Linux Systems"
-			sudo pacman -S --noconfirm bleachbit rmlint
+			echo "1 - bleachbit and rmlint"
+			echo "2 - kde sweeper"
+			read package
+			if [[ $package == 1 ]];
+			then
+				sudo pacman -S --noconfirm bleachbit rmlint
+			elif [[ $package == 2 ]];
+			then
+				sudo pacman -S sweeper
+			else
+				echo "You've entered an invalid number"
+				InstallAndConquer
+			fi
 			;;
 			8)
 			echo "This installs a prelauncher"
@@ -881,7 +893,7 @@ InstallAndConquer(){
 				wget https://aur.archlinux.org/cgit/aur.git/snapshot/vivaldi-snapshot.tar.gz; gunzip vivaldi-snapshot.tar.gz; tar -xvf vivaldi-snapshot.tar; cd vivaldi-snapshot && makepkg -si
 			elif [[ $browser == 8 ]];
 			then
-				wget https://linux.palemoon.org/datastore/release/palemoon-29.1.1.linux-x86_64-gtk3.tar.xz; sudo ln -s ~/palemoon/palemoon /usr/bin/palemoon
+				wget https://linux.palemoon.org/datastore/release/palemoon-29.1.1.linux-x86_64-gtk3.tar.xz; tar -xvf palemoon-29.1.1.linux-x86_64-gtk3.tar.xz; sudo ln -s ~/palemoon/palemoon /usr/bin/palemoon
 				wget https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/palemoon.desktop; sudo mv palemoon.desktop /usr/share/applications/palemoon.desktop
 			elif [[ $browser == 9 ]];
 			then
@@ -897,7 +909,7 @@ InstallAndConquer(){
 				wget https://aur.archlinux.org/cgit/aur.git/snapshot/google-chrome.tar.gz; gunzip google-chrome.tar.gz; tar -xvf google-chrome.tar; cd google-chrome && makepkg -si
 			elif [[ $browser == 13 ]];
 			then
-				wget https://cdn.waterfox.net/releases/linux64/installer/waterfox-G3.2.0.en-US.linux-x86_64.tar.bz2; gunzip waterfox-G3.2.0.en-US.linux-x86_64.tar.bz2; tar -xvf waterfox-G3.1.0.en-US.linux-x86_64.tar; sudo ln -s ~/waterfox/waterfox /usr/bin/waterfox
+				wget https://cdn.waterfox.net/releases/linux64/installer/waterfox-G3.2.1.en-US.linux-x86_64.tar.bz2; gunzip waterfox-G3.2.1.en-US.linux-x86_64.tar.bz2; tar -xvf waterfox-G3.1.0.en-US.linux-x86_64.tar; sudo ln -s ~/waterfox/waterfox /usr/bin/waterfox
 				wget https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/waterfox.desktop && sudo mv waterfox.desktop /usr/share/applications/waterfox.desktop
 			elif [[ $browser == 14 ]];
 			then
