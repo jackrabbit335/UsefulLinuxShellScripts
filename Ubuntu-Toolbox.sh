b9c40b7f2c7099f6235e779115d3dbe541dee2b8
James <VeilofMaya@vivaldi.net>
2021-06-14 03:14:27 -0500

    Update Ubuntu-Toolbox.sh
    Same as Archh-Toolbox

--- Ubuntu-Toolbox.sh
+++ Ubuntu-Toolbox.sh
@@ -1070,7 +1070,7 @@ InstallAndConquer(){
 			elif [[ $browser == 6 ]];
 			then
 				wget https://linux.palemoon.org/datastore/release/palemoon-29.2.1.linux-x86_64-gtk3.tar.xz; tar -xvf palemoon-29.2.1.linux-x86_64-gtk3.tar.xz; sudo ln -s ~/palemoon/palemoon /usr/bin/palemoon
-				wget https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/palemoon.desktop; sudo mv palemoon.desktop /usr/share/applications/palemoon.desktop
+				wget https://raw.githubusercontent.com/jackrabbit335/BrowserAndDesktop/main/palemoon.desktop; sudo mv palemoon.desktop /usr/share/applications/palemoon.desktop
 			elif [[ $browser == 7 ]];
 			then
 				wget https://downloads.vivaldi.com/stable/vivaldi-stable_4.0.2312.27-1_amd64.deb; sudo apt install -f
@@ -1086,11 +1086,11 @@ InstallAndConquer(){
 			elif [[ $browser == 11 ]];
 			then
 				wget https://cdn.waterfox.net/releases/linux64/installer/waterfox-G3.2.2.en-US.linux-x86_64.tar.bz2; waterfox-G3.2.2.en-US.linux-x86_64.tar.bz2; sudo ln -s ~/waterfox/waterfox /usr/bin/waterfox; sudo mv waterfox /opt && sudo ln -s /opt/waterfox/waterfox /usr/bin/waterfox
-				wget https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/waterfox.desktop; sudo mv waterfox.desktop /usr/share/applications/waterfox.desktop
+				wget https://raw.githubusercontent.com/jackrabbit335/BrowserAndDesktop/main/waterfox.desktop; sudo mv waterfox.desktop /usr/share/applications/waterfox.desktop
 			elif [[ $browser == 12 ]];
 			then
 				wget https://us.basilisk-browser.org/release/basilisk-latest.linux64.tar.xz; tar -xvf basilisk-latest.linux64.tar.xz; sudo mv basilisk /opt && sudo ln -s /opt/basilisk/basilisk /usr/bin/basilisk
-				wget https://raw.githubusercontent.com/jackrabbit335/UsefulLinuxShellScripts/master/basilisk.desktop; sudo mv basilisk.desktop /usr/share/applications/basilisk.desktop
+				wget https://raw.githubusercontent.com/jackrabbit335/BrowserAndDesktop/main/basilisk.desktop; sudo mv basilisk.desktop /usr/share/applications/basilisk.desktop
 			elif [[ $browser == 13 ]];
 			then
 				sudo snap install brave
