33a140e2596adba273abadc83011ad92669bafb6
James <VeilofMaya@vivaldi.net>
2021-07-03 04:53:38 -0500

    Add files via upload
    Same as Arch

--- Ubuntu-Toolbox.sh
+++ Ubuntu-Toolbox.sh
@@ -1442,6 +1442,27 @@ CheckNetwork(){
 	done
 }
 
+Disclaimer(){
+cat <<EOF
+##########################################################################
+Hello! Thank you for using Arch Toolbox. Within this script is a multitu-
+de of potential solutions for every day tasks as trivial as maintenance,
+all the way to as important as setting up a new system. This script is
+meant for new users, but anyone can read, change and use this script to
+their liking. This script is to be placed under the GPLv3 and is to be
+redistributable, however, if you are distributing, I would appreciate it
+if you gave the credit back to the original author. I should also add that
+I have a few blog articles which may or may not be of benefit for newbies
+on occasion. The link will be placed here. In the blog I write about
+typical scenarios that I face on a day to day basis as well as add commen-
+tary and my opinions about software and technology. You may copy and paste
+the following link into your browser: https://techiegeek123.blogspot.com/
+Again, Thank you!
+##########################################################################
+EOF
+read -p  $'\n'$"Press enter to continue..."
+}
+
 Adblocking(){
 	find Hostsman4linux.sh
 	while [ $? -eq 1 ];
@@ -1879,24 +1900,6 @@ EOF
 }
 
 Greeting(){
-cat <<EOF
-##########################################################################
-Hello! Thank you for using Arch Toolbox. Within this script is a multitu-
-de of potential solutions for every day tasks as trivial as maintenance,
-all the way to as important as setting up a new system. This script is
-meant for new users, but anyone can read, change and use this script to
-their liking. This script is to be placed under the GPLv3 and is to be
-redistributable, however, if you are distributing, I would appreciate it
-if you gave the credit back to the original author. I should also add that
-I have a few blog articles which may or may not be of benefit for newbies
-on occasion. The link will be placed here. In the blog I write about
-typical scenarios that I face on a day to day basis as well as add commen-
-tary and my opinions about software and technology. You may copy and paste
-the following link into your browser: https://techiegeek123.blogspot.com/
-Again, Thank you!
-##########################################################################
-EOF
-read -p  $'\n'$"Press enter to continue..."
 	echo $'\n'$"Enter a selection from the following list"
 	echo "1 - Setup your system"
 	echo "2 - Add/Remove user accounts"
@@ -1990,4 +1993,5 @@ read -p  $'\n'$"Press enter to continue..."
 	esac
 }
 
+Disclaimer
 Greeting
