52bc7fbd6406f9eedf22da7b75ba5bb57fa453a1
James <VeilofMaya@vivaldi.net>
2021-08-17 15:14:35 -0500

    Update Ubuntu-Toolbox.sh
    Updated Palemoon

--- Ubuntu-Toolbox.sh
+++ Ubuntu-Toolbox.sh
@@ -1069,7 +1069,7 @@ InstallAndConquer(){
 				cd /tmp; wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb; sudo dpkg -i *.deb; sudo apt install -f
 			elif [[ $browser == 6 ]];
 			then
-				wget https://linux.palemoon.org/datastore/release/palemoon-29.3.0.linux-x86_64-gtk3.tar.xz; tar -xvf palemoon-29.3.0.linux-x86_64-gtk3.tar.xz; sudo ln -s ~/palemoon/palemoon /usr/bin/palemoon
+				wget https://linux.palemoon.org/datastore/release/palemoon-29.4.0.linux-x86_64-gtk3.tar.xz; tar -xvf palemoon-29.4.0.linux-x86_64-gtk3.tar.xz; sudo ln -s ~/palemoon/palemoon /usr/bin/palemoon
 				wget https://raw.githubusercontent.com/jackrabbit335/BrowserAndDesktop/main/palemoon.desktop; sudo mv palemoon.desktop /usr/share/applications/palemoon.desktop
 			elif [[ $browser == 7 ]];
 			then
