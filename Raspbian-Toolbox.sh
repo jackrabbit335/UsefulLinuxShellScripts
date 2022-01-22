67a83b208febd1e8fc3af2f5f680388b4344c03a
James <VeilofMaya@vivaldi.net>
2022-01-22 11:32:18 -0600

    Update Raspbian-Toolbox.sh
    Update Vivaldi

--- Raspbian-Toolbox.sh
+++ Raspbian-Toolbox.sh
@@ -189,7 +189,7 @@ InstallAndConquer(){
 				sudo apt install -y epiphany-browser
 			elif [[ $package == 4 ]];
 			then
-				wget https://downloads.vivaldi.com/stable/vivaldi-stable_5.0.2497.38-1_armhf.deb; sudo dpkg -i *.deb; sudo apt install -f
+				wget https://downloads.vivaldi.com/stable/vivaldi-stable_5.0.2497.48-1_armhf.deb; sudo dpkg -i *.deb; sudo apt install -f
 			elif [[ $package == 5 ]];
 			then
 				sudo apt install -y netsurf
