bb493264a5fddd79cfa70d30a2e1f11885a94f0f
James <VeilofMaya@vivaldi.net>
2022-01-27 08:53:44 -0600

    Update Raspbian-Toolbox.sh
    Removed unnecessary extra alias

--- Raspbian-Toolbox.sh
+++ Raspbian-Toolbox.sh
@@ -64,7 +64,6 @@ Setup(){
 	echo 'alias meminfo="cat /proc/meminfo"' >> ~/.bashrc
 	echo 'alias mem="watch free -h"' >> ~/.bashrc
 	echo 'alias temp="watch vcgencmd measure_temp"' >> ~/.bashrc
-	echo 'alias cpuinfo="cat /proc/cpuinfo"' >> ~/.bashrc
 	echo 'alias cur_freq="cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq"' >> ~/.bashrc
 	echo 'alias min_freq="cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq"' >> ~/.bashrc
 	echo 'alias max_freq="cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"' >> ~/.bashrc
