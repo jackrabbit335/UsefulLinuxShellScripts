f3407b93768ba34a268f5fd2e1e51106d34a0438
James <VeilofMaya@vivaldi.net>
2022-04-03 07:38:25 -0500

    Update Ubuntu-Toolbox.sh
    Updated Pale Moon to fix caching issue

--- Ubuntu-Toolbox.sh
+++ Ubuntu-Toolbox.sh
@@ -1155,7 +1155,7 @@ InstallAndConquer(){
 				cd /tmp; wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb; sudo dpkg -i *.deb; sudo apt install -f
 			elif [[ $browser == 6 ]];
 			then
-				wget http://linux.palemoon.org/datastore/release/palemoon-29.4.5.linux-x86_64-gtk3.tar.xz; tar -xvf palemoon-29.4.5.linux-x86_64-gtk3.tar.xz; sudo ln -s ~/palemoon/palemoon /usr/bin/palemoon
+				wget http://linux.palemoon.org/datastore/release/palemoon-29.4.5.1.linux-x86_64-gtk3.tar.xz; tar -xvf palemoon-29.4.5.1.linux-x86_64-gtk3.tar.xz; sudo ln -s ~/palemoon/palemoon /usr/bin/palemoon
 				wget https://raw.githubusercontent.com/jackrabbit335/BrowserAndDesktop/main/palemoon.desktop; sudo mv palemoon.desktop /usr/share/applications/palemoon.desktop
 			elif [[ $browser == 7 ]];
 			then
