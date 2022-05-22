d2929492eaf7ead9901fbac0fb4d1859e253c768
James <VeilofMaya@vivaldi.net>
2022-05-22 06:22:40 -0500

    Update Raspbian-Toolbox.sh
    Updated Vivaldi

--- Raspbian-Toolbox.sh
+++ Raspbian-Toolbox.sh
@@ -219,9 +219,9 @@ InstallAndConquer(){
 				architecture=$(lscpu | grep Architecture | awk '{print $2}')
 				if [[ $architecture == aarch64 ]];
 				then
-					wget https://downloads.vivaldi.com/stable/vivaldi-stable_5.2.2623.46-1_arm64.deb; sudo dpkg -i *.deb; sudo apt install -f
+					wget https://downloads.vivaldi.com/stable/vivaldi-stable_5.2.2623.48-1_arm64.deb; sudo dpkg -i *.deb; sudo apt install -f
 				else	
-					wget https://downloads.vivaldi.com/stable/vivaldi-stable_5.2.2623.46-1_armhf.deb; sudo dpkg -i *.deb; sudo apt install -f
+					wget https://downloads.vivaldi.com/stable/vivaldi-stable_5.2.2623.48-1_armhf.deb; sudo dpkg -i *.deb; sudo apt install -f
 				fi
 				
 			elif [[ $package == 5 ]];
