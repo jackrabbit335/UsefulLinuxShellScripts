92956a3836b05a38735c52de14b1a7a9d16e17f2
James <VeilofMaya@vivaldi.net>
2022-07-20 01:11:46 -0500

    Update Ubuntu-Toolbox.sh
    updated Waterfox

--- Ubuntu-Toolbox.sh
+++ Ubuntu-Toolbox.sh
@@ -1172,7 +1172,7 @@ InstallAndConquer(){
 				sudo apt install -y dillo
 			elif [[ $browser == 11 ]];
 			then
-				wget https://github.com/WaterfoxCo/Waterfox/releases/download/G4.1.3.1/waterfox-G4.1.3.1.en-US.linux-x86_64.tar.bz2; tar -xf waterfox-G4.1.3.1.en-US.linux-x86_64.tar.bz2; sudo ln -s ~/waterfox/waterfox /usr/bin/waterfox; sudo mv waterfox /opt && sudo ln -s /opt/waterfox/waterfox /usr/bin/waterfox
+				wget https://github.com/WaterfoxCo/Waterfox/releases/download/G4.1.3.1/waterfox-G4.1.3.2.en-US.linux-x86_64.tar.bz2; tar -xf waterfox-G4.1.3.2.en-US.linux-x86_64.tar.bz2; sudo ln -s ~/waterfox/waterfox /usr/bin/waterfox; sudo mv waterfox /opt && sudo ln -s /opt/waterfox/waterfox /usr/bin/waterfox
 				wget https://raw.githubusercontent.com/jackrabbit335/BrowserAndDesktop/main/waterfox.desktop; sudo mv waterfox.desktop /usr/share/applications/waterfox.desktop
 			elif [[ $browser == 12 ]];
 			then
