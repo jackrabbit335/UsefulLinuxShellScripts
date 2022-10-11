fa6d4ea021484a94d5c6e9f157c2280e7a7e611a
James <VeilofMaya@vivaldi.net>
2022-10-11 06:09:22 -0500

    Update Solus-Toolbox.sh
    Updated Palemoon

--- Solus-Toolbox.sh
+++ Solus-Toolbox.sh
@@ -744,7 +744,7 @@ InstallAndConquer(){
 				wget https://raw.githubusercontent.com/jackrabbit335/BrowserAndDesktop/main/waterfox.desktop; sudo mv waterfox.desktop /usr/share/applications/waterfox.desktop
 			elif [[ $browser == 10 ]];
 			then
-				wget https://linux.palemoon.org/datastore/release/palemoon-31.3.0.linux-x86_64-gtk3.tar.xz; tar -xvf palemoon-31.3.0.linux-x86_64-gtk3.tar.xz; sudo ln -s ~/palemoon/palemoon /usr/bin/palemoon
+				wget https://linux.palemoon.org/datastore/release/palemoon-31.3.0.1.linux-x86_64-gtk3.tar.xz; tar -xvf palemoon-31.3.0.1.linux-x86_64-gtk3.tar.xz; sudo ln -s ~/palemoon/palemoon /usr/bin/palemoon
 				wget https://raw.githubusercontent.com/jackrabbit335/BrowserAndDesktop/main/palemoon.desktop; sudo mv palemoon.desktop /usr/share/applications/palemoon.desktop
 			elif [[ $browser == 11 ]];
 			then
